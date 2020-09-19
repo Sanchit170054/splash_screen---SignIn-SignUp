dbaa4fdc94d03239aa8eb23e6fdb067beb12d259
Sanchit170054 <psanchit218@gmail.com>
2020-09-19 17:33:52 +0530

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:splash_screen/login.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 void main() {
@@ -25,7 +26,7 @@ class SplashScreenPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SplashScreen(
       seconds: 5,
-      navigateAfterSeconds: new SecondScreen(),
+      navigateAfterSeconds: new LoginPage(),
       backgroundColor: Colors.black,
       title: new Text('Welcome to MoviSic',
           textScaleFactor: 2,
@@ -46,7 +47,7 @@ class SecondScreen extends StatelessWidget {
       appBar: AppBar(title: Text("Home Screen")),
       body: Center(
           child: Text(
-        "Sample Page",
+        "Home page",
         textScaleFactor: 2,
       )),
     );
